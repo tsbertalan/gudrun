de335855181c6f1bcaee8a577b8bd9bd05d31ebc
Tom Bertalan <tom@tombertalan.com>
2019-04-05 00:09:25 -0400

    Clean up python mc class.

--- src/gudrun_motor/motor_control/motor_control.py
+++ src/gudrun_motor/motor_control/motor_control.py
@@ -1,47 +1,45 @@
 from __future__ import print_function
 import serial, time
-
 from subprocess import check_output
 
-PORT = check_output(['rosrun', 'gudrun_sensors', 'get_usb_device_by_ID.py', 'motor_control']).strip()
-BAUDRATE = 115200
+class MotorControl(object):
 
-ser = serial.Serial(PORT, BAUDRATE)
+    def __init__(self, PORT=None, BAUDRATE=115200):
 
-def checksum(msg):
-    t = 0
-    for c in msg:
-        if isinstance(msg, str):
-            t += ord(c)
-        else:
+        if PORT is None: PORT = check_output(['rosrun', 'gudrun_sensors', 'get_usb_device_by_ID.py', 'motor_control']).strip()
+
+        self.ser = serial.Serial(PORT, BAUDRATE)
+
+        self.HEADER = 0x7E
+
+    @staticmethod
+    def checksum(msg):
+        t = 0
+        for c in msg:
+            # if isinstance(msg, str):
+            #     t += ord(c)
+            # else:
             t += c
-    return t & 0xff
+        return t & 0xff
 
-HEADER = 0x7E
+    def send_packet(self, byte_a, byte_b):
+        c = self.checksum([self.HEADER, byte_a, byte_b])
+        print('Sending a=%s, b=%s; checksum=%s' % (byte_a, byte_b, c))
+        self.ser.write(chr(self.HEADER))
+        self.ser.write(chr(byte_a))
+        self.ser.write(chr(byte_b))
+        self.ser.write(chr(c))
+        time.sleep(.01)
 
-def write(c):
-    # print('Writing "%s"' % c, end=' ')
-    b = ser.write(chr(c))
-    # print('(%d bytes)' % b)
+        print('Response:')
+        response = []
+        while self.ser.in_waiting > 0:
+            data = self.ser.readline()
+            response.append(data)
+        print(''.join(['    %s' % r for r in response]))
+        print('%d lines' % (len(response),))
 
 
-def send(a, b):
-    c = checksum([HEADER, a, b])
-    print('Sending a=%s, b=%s; checksum=%s' % (a, b, c))
-    write(HEADER)
-    write(a)
-    write(b)
-    write(c)
-    write(42)
-    time.sleep(.01)
+mc = MotorControl()
+mc.send_packet(0, 90)
 
-    print('Response:')
-    response = []
-    while ser.in_waiting > 0:
-        data = ser.readline()
-        response.append(data)
-    print(''.join(['    %s' % r for r in response]))
-    print('%d lines' % (len(response),))
-
-write(42)
-send(180, 180)
