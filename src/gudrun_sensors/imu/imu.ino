e2becd57df940941e0dabddb394755f33e9c14ea
Tom Bertalan <tom@tombertalan.com>
2019-04-13 10:36:53 -0400

    Remove some commented-out stuff from IMU firmware.
    Also, if wiring is bad, latch on complaint.

--- src/gudrun_sensors/imu/imu.ino
+++ src/gudrun_sensors/imu/imu.ino
@@ -23,18 +23,25 @@ void initSensors()
   if(!accel.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no accel detected ... Check your wiring!"));
-    while(1);
+    while(1) {
+      Serial.println(F("Ooops, no accel detected ... Check your wiring!"));
+      delay(1000);
+    }
   }
   if(!mag.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no mag detected ... Check your wiring!");
-    while(1);
+    while(1) {
+      Serial.println("Ooops, no mag detected ... Check your wiring!");
+      delay(1000);
+    }
   }
   if(!gyro.begin())
   {
-    Serial.println("Oops, no gyro detected ... Check your wiring!");
+    while(1) {
+      Serial.println("Oops, no gyro detected ... Check your wiring!");
+      delay(1000);
+    }
   }
 }
 
@@ -71,57 +78,11 @@ void loop(void)
 
   DataPackage package;
 
-  // /* Calculate pitch and roll from the raw accelerometer data */
-  // accel.getEvent(&accel_event);
-
-  // Serial.print(accel_event.acceleration.x);
-  // Serial.print(F(", "));
-  // Serial.print(accel_event.acceleration.y);
-  // Serial.print(F(", "));
-  // Serial.println(accel_event.acceleration.z);
-
-  // if (dof.accelGetOrientation(&accel_event, &orientation))
-  // {
-  //   /* 'orientation' should have valid .roll and .pitch fields */
-  //   Serial.print(F("Roll: "));
-  //   Serial.print(orientation.roll);
-  //   Serial.print(F("; "));
-  //   Serial.print(F("Pitch: "));
-  //   Serial.print(orientation.pitch);
-  //   Serial.print(F("; "));
-  // }
-  
-  // /* Calculate the heading using the magnetometer */
-  // mag.getEvent(&mag_event);
-  // if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-  // {
-  //   /* 'orientation' should have valid .heading data now */
-  //   Serial.print(F("Heading: "));
-  //   Serial.print(orientation.heading);
-  //   Serial.print(F("; "));
-  // }
-  // Serial.println(F(""));
-
-
-
   accel.getEvent(&accel_event);
   mag.getEvent(&mag_event);
   gyro.getEvent(&gyro_event);
 
-  // Serial.print(accel_event.acceleration.x, 4);
-  // Serial.print(F(","));
-  // Serial.print(accel_event.acceleration.y, 4);
-  // Serial.print(F(","));
-  // Serial.print(accel_event.acceleration.z, 4);
-  // Serial.print(F(","));
-
-  // Serial.print(gyro_event.gyro.x, 4);
-  // Serial.print(F(","));
-  // Serial.print(gyro_event.gyro.y, 4);
-  // Serial.print(F(","));
-  // Serial.print(gyro_event.gyro.z, 4);
-  
-  package.ax = accel_event.acceleration.x;
+    package.ax = accel_event.acceleration.x;
   package.ay = accel_event.acceleration.y;
   package.az = accel_event.acceleration.z;
 
@@ -131,13 +92,6 @@ void loop(void)
 
 
   if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation)) {
-    // Serial.print(F(","));
-    // Serial.print(orientation.roll);
-    // Serial.print(F(","));
-    // Serial.print(orientation.pitch);
-    // Serial.print(F(","));
-    // Serial.print(orientation.heading);
-
     package.roll = orientation.roll;
     package.pitch = orientation.pitch;
     package.heading = orientation.heading;
@@ -145,10 +99,10 @@ void loop(void)
     Serial.write('S');
     Serial.write((byte*)&package, sizeof(package));
     Serial.write('E');
+
+    // Serial.print(F("Data package roll:"));
+    // Serial.println(orientation.roll);
   }
-
-  // Serial.println();
-
   
   delayMicroseconds(100);
 }
