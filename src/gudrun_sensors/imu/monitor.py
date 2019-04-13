9a19778c4f8649305e182b2b866e606f879075ed
Tom Bertalan <tom@tombertalan.com>
2019-04-12 23:10:11 -0400

    Let our former __main__block be a monitor.py script.

---
+++ src/gudrun_sensors/imu/monitor.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import time, datetime
+import numpy as np
+from imu_driver import IMU
+
+imu = IMU()
+
+def get_time_micros():
+    return int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() * 1000000)
+    return(int(round(time.time() * 1000000)))
+    
+micros = get_time_micros()
+
+for values in imu.stream():
+
+    values = tuple(values)
+
+    current_time = get_time_micros()
+    time_elapsed = current_time - micros
+    micros = current_time
+
+    print("Time elapsed since last (us): " + str(time_elapsed))
+    print("------")
+    print("Accelerations: %.2f %.2f %.2f" % values[0:3])
+    print("Angular rates: %.2f %.2f %.2f" % values[3:6])
+    print("Orientation:   %.2f %.2f %.2f" % values[6:9])
+    print(u"{}[2J{}[;H".format(chr(27), chr(27)), end="")  # clear screen https://stackoverflow.com/a/2084521/1224886
--- src/gudrun_sensors/imu/monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SEARCH_STRING=$( cat device_search_string.txt )
-# trim whitespace
-# SEARCH_STRING=${SEARCH_STRING##*( )}
-port=$( rosrun  gudrun_sensors get_usb_device_by_ID.py "$SEARCH_STRING" )
-screen $port 
