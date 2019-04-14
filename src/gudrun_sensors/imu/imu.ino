e7357bc0a1dd12771b5f1e7886fe6c67bcdcebac
Tom Bertalan <tom@tombertalan.com>
2019-04-13 23:33:03 -0400

    Use Madgwick's IMU filter instead of the Adafruit firmware one.
    Leave out the magnetometer for now.

--- src/gudrun_sensors/imu/imu.ino
+++ src/gudrun_sensors/imu/imu.ino
@@ -4,22 +4,15 @@
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_9DOF.h>
 
+
 /* Assign a unique ID to the sensors */
 Adafruit_9DOF                 dof   = Adafruit_9DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(30303);
 
-/* Update this with the correct SLP for accurate altitude measurements */
-float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
-/**************************************************************************/
-/*!
-    @brief  Initialises all the sensors used by this example
-*/
-/**************************************************************************/
-void initSensors()
-{
+void initSensors() {
   if(!accel.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
@@ -45,64 +38,48 @@ void initSensors()
   }
 }
 
-/**************************************************************************/
-/*!
 
-*/
-/**************************************************************************/
-void setup(void)
-{
+void setup(void) {
   Serial.begin(115200);
-  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading Example")); Serial.println("");
-  
-  /* Initialise the sensors */
   initSensors();
 }
 
 
-/**************************************************************************/
-/*!
-    @brief  Constantly check the roll/pitch/heading/altitude/temperature
-*/
-/**************************************************************************/
-void loop(void)
-{
+void loop(void) {
+
   sensors_event_t accel_event;
   sensors_event_t mag_event;
   sensors_event_t gyro_event;
   sensors_vec_t   orientation;
 
   typedef struct {
-    float ax, ay, az, gx, gy, gz, roll, pitch, heading;
+    //    acceleration, gyro,     magnetometer
+    float ax, ay, az, gx, gy, gz, mx, my, mz;
   } DataPackage;
-
   DataPackage package;
 
-  accel.getEvent(&accel_event);
-  mag.getEvent(&mag_event);
-  gyro.getEvent(&gyro_event);
+  if(
+    accel.getEvent(&accel_event)
+    && 
+    mag.getEvent(&mag_event)
+    && 
+    gyro.getEvent(&gyro_event)
+    ) {
 
     package.ax = accel_event.acceleration.x;
-  package.ay = accel_event.acceleration.y;
-  package.az = accel_event.acceleration.z;
+    package.ay = accel_event.acceleration.y;
+    package.az = accel_event.acceleration.z;
 
-  package.gx = gyro_event.gyro.x;
-  package.gy = gyro_event.gyro.y;
-  package.gz = gyro_event.gyro.z;
+    package.gx = gyro_event.gyro.x;
+    package.gy = gyro_event.gyro.y;
+    package.gz = gyro_event.gyro.z;
 
+    package.mx = mag_event.magnetic.x;
+    package.my = mag_event.magnetic.y;
+    package.mz = mag_event.magnetic.z;
 
-  if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation)) {
-    package.roll = orientation.roll;
-    package.pitch = orientation.pitch;
-    package.heading = orientation.heading;
-  
     Serial.write('S');
     Serial.write((byte*)&package, sizeof(package));
     Serial.write('E');
-
-    // Serial.print(F("Data package roll:"));
-    // Serial.println(orientation.roll);
   }
-  
-  delayMicroseconds(100);
 }
--- src/gudrun_sensors/imu/imu_driver.py
+++ src/gudrun_sensors/imu/imu_driver.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+"""Read data continuously from Adafruit 9 DoF "IMU".
 
-"""Thanks to Jean Rabault:
+Arduino struct-passing method thanks to Jean Rabault:
 https://folk.uio.no/jeanra/Microelectronics/TransmitStructArduinoPython.html"""
 
 from __future__ import print_function
 from serial import Serial
+from serial.serialutil import SerialException
 import struct
 
 
@@ -38,11 +40,15 @@ class IMU(object):
     """A class to stream the serial messages from Arduino."""
 
     def __init__(self, SIZE_STRUCT=9*4, verbose=0):
-        self.port = Serial(get_port_address(), 115200)
-        
         self.SIZE_STRUCT = SIZE_STRUCT
         self.verbose = verbose
-        
+        try:
+            self.connect()
+        except IOError:
+            self.attempt_reconnect()
+
+    def connect(self):
+        self.port = Serial(get_port_address(), 115200)
         self.port.flushInput()
 
     def read_one_value(self):
@@ -50,67 +56,171 @@ class IMU(object):
         and read the whole message as a structure."""
         read = False
 
-        while not read:
-            myByte = self.port.read(1)
-            if myByte == 'S':
-                data = self.port.read(self.SIZE_STRUCT)
+        try:
+
+            while not read:
                 myByte = self.port.read(1)
-                if myByte == 'E':
+                if myByte == 'S':
+                    data = self.port.read(self.SIZE_STRUCT)
+                    myByte = self.port.read(1)
+                    if myByte == 'E':
 
-                    # is  a valid message struct
-                    new_values = list(struct.unpack('<fffffffff', data))
+                        # is  a valid message struct
+                        new_values = list(struct.unpack('<fffffffff', data))
 
-                    read = True
+                        read = True
 
-                    # convert measurements to rad/s
-                    for i in range(3, 6):
-                        new_values[i] = 3.1415 / 180.0 * new_values[i]
+                        # sensors_vec_t   acceleration;         /**< acceleration values are in meter per second per second (m/s^2) */
+                        # sensors_vec_t   gyro;                 /**< gyroscope values are in rad/s */
+                        # sensors_vec_t   magnetic;             /**< magnetic vector values are in micro-Tesla (uT) */
+                        # sensors_vec_t   orientation;          /**< orientation values are in degrees */
 
-                    return new_values
+                        # convert micro-Tesla to Tesla
+                        for i in range(6, 9):
+                            new_values[i] = new_values[i] / 1000.
+
+                        return new_values
+
+        except SerialException as e:
+            self.attempt_reconnect()
 
         return None
 
     def stream(self):
+        """Convenience loop reading packets continuously.
+
+        e.g.
+        >>> imu = IMU()
+        >>> for packet in imu.stream():
+        ...     ax, ay, az = packet[:3]
+        """
         while True:
             yield self.read_one_value()
 
+    def attempt_reconnect(self):
+        """Try to recoonnect repeatedly, with limited exponential backoff."""
+        from warnings import warn
+        from time import sleep
+
+        backoff = .1
+        backoff_max = 10
+
+        while True:
+
+            msg = 'Disconnected? Attempting reconnection in %s seconds (max %s).' % (backoff, backoff_max)
+            # warn(msg)
+            print(msg)
+            sleep(backoff)
+
+            try:
+                self.connect()
+                print('Connected!')
+                break
+
+            except IOError:
+                backoff = min(1.5 * backoff, backoff_max)
+
 
 def ros_publish(rate=None):
+    """Send the data to ROS."""
     import rospy
-    from sensor_msgs.msg import Imu as Imu_Message
+    from sensor_msgs.msg import Imu, MagneticField
+
+    from std_msgs.msg import Float32
+    from math import sqrt
 
     FRAME_NAME = 'imu'
 
     rospy.init_node('imu')
 
-    publisher = rospy.Publisher('imu', Imu_Message, queue_size=10)
+    # Set up our publishers and persistent message objects.
+    def set_cov_diagonal(arr, s):
+        """Fill out a covariance matrix along its diagonal."""
+        try:
+            s = list(s)
+        except TypeError:
+            s = [s]*3
+        for i, si in zip([0, 4, 8], s):
+            arr[i] = si
 
-    msg = Imu_Message()
+    # Apparently the rule-of-thumb is that you can use your publishing rate as a rough
+    # guess for what your queue size should be? My rate is more like 250 Hz, though, so idunno.
+    estimated_frequency = rate or 100
+
+    # Publish the linear accelerations and angular velocities.
+    publisher_imu_data_raw = rospy.Publisher('imu/data_raw', Imu, queue_size=estimated_frequency)
+    msg_imu_data_raw = Imu()
+    msg_imu_data_raw.header.frame_id = FRAME_NAME
+
+    # We don't publish orientation in this pseudo-Imu topic, so, per the docs, 
+    # we set the first element of the corresponding covariance matrix to -1.
+    msg_imu_data_raw.orientation_covariance[0] = -1  
+
+    # Publish the magnetometer.
+    publisher_imu_mag = rospy.Publisher('imu/mag', MagneticField, queue_size=estimated_frequency)
+    msg_imu_mag = MagneticField()
+    msg_imu_mag.header.frame_id = FRAME_NAME
+
+    # I got these covariance numbers in a super-scientific way by watching rqt
+    # and eyeballing the typical range of quiescent variation.
+    set_cov_diagonal(msg_imu_data_raw.linear_acceleration_covariance, 0.15)
+    set_cov_diagonal(msg_imu_data_raw.angular_velocity_covariance, 0.005)
+    set_cov_diagonal(msg_imu_mag.magnetic_field_covariance, 0.0007)
+
+    # Monitoring this topic will convince you that the static field of the motor's permanent magnet is fierce.
+    # It might be relatively constant, though.
+    # TODO: Do hard-iron magnetometer compensation.
+    publisher_mag_magnitude = rospy.Publisher('imu/mag_magnitude', Float32, queue_size=estimated_frequency)
 
     imu = IMU()
 
-
+    # Maybe we throttle; maybe not. Not sure what's best practice here.
+    # Also, if we don't read, will earlier piece of the pipeline from the sensor chips buffer??
+    # I don't think we want that.
     if rate is not None:
         dt = 1. / rate
-        t_last = rospy.get_time() - dt - 1
+        t_last = rospy.get_time() - dt - 1  # Ensure we'll publish on the first loop.
 
-    while not rospy.is_shutdown():
+    for data in imu.stream():
+        if rospy.is_shutdown():
+            break
 
-        data = imu.read_one_value()
+        # Sometimes the IMU will return None.
+        if data:
 
-        if rate is not None: t = rospy.get_time()
+            if rate is not None: t = rospy.get_time()
 
-        if rate is None or t >= t_last + dt:
-            msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z = data[0:3]
-            msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z = data[3:6]
-            msg.orientation.x, msg.orientation.y, msg.orientation.z = data[6:9]
+            if rate is None or t >= t_last + dt:
 
-            msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = FRAME_NAME
+                # First three data values are linear accelerations.
+                msg = msg_imu_data_raw
+                publish = publisher_imu_data_raw.publish
+                msg_imu_data_raw.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z = data[0:3]
 
-            publisher.publish(msg)
+                # Next three are angular velocities.
+                msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z = data[3:6]
 
-            t_last = rospy.get_time()
+                # Apply emperical anti-drift offsets.
+                # These were obtained by letting the car sit still on a desk, and watching rqt.
+                msg.angular_velocity.x -= .035
+                msg.angular_velocity.y -= .0125
+                msg.angular_velocity.z -= .01625
+
+                # Stamp and publish the linear acceleration/angular velocity pseudo-Imu message.
+                msg.header.stamp = rospy.Time.now()
+                publish(msg)
+
+                # Final three values are magnetometer.
+                msg = msg_imu_mag
+                publish = publisher_imu_mag.publish
+                msg.magnetic_field.x, msg.magnetic_field.y, msg.magnetic_field.z = data[6:9]
+                msg.header.stamp = rospy.Time.now()
+                publish(msg)
+
+                # For debugging purposes, also publish the magnitude of the magnetic field vector.
+                publisher_mag_magnitude.publish(sqrt(sum([c**2 for c in data[6:9]])))
+
+                t_last = rospy.get_time()
 
 
 if __name__ == '__main__':
