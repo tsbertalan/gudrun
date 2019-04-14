81c25b333fd2a94d3f6259075e71ded3ecaacac7
Tom Bertalan <tom@tombertalan.com>
2019-04-14 15:59:29 -0400

    Clean up encoder firmware a little.

--- src/gudrun_motor/encoder/encoder.ino
+++ src/gudrun_motor/encoder/encoder.ino
@@ -1,41 +1,31 @@
 volatile long count;
-volatile byte last_symbol;
-
-// volatile long millis_last_count;
-// volatile double counts_per_second;
-// unsigned long timeold;
-
-// double dt, rpm, wheel_revolutions, wheel_revolutions_old, last_printed_wheel_revolutions;
+volatile byte last_symbol, symbol, a, b;
 
 const int PIN_A = 2;
 const int PIN_B = 3;
 
-const byte fwd_sources[4] = {2, 0, 3, 1};
-const byte rev_sources[4] = {1, 3, 0, 2};
+// There are two distinct seminibble sequences;
+// one for forward, one reverse.
+// Current byte     ->       0b00, 0b01, 0b10, 0b11
+// Previous byte:
+const byte fwd_sources[4] = {0b10, 0b00, 0b11, 0b01};
+const byte rev_sources[4] = {0b01, 0b11, 0b00, 0b10};
 
 void isr() {
+	// We enter here whenever a rising or falling edge is seen on either channel.
 
-	byte a = digitalRead(PIN_A);
-	byte b = digitalRead(PIN_B);
+	a = digitalRead(PIN_A);
+	b = digitalRead(PIN_B);
 
-	// Serial.print("0b");
-	// Serial.print(a);
-	// Serial.print(b);
-
-	byte symbol = b + (a << 1);
-	// Serial.print(" = "); Serial.println(symbol, DEC);
+	symbol = b + (a << 1);
 
 	if(fwd_sources[symbol] == last_symbol) {
-		// Serial.println("Forward.");
 		count++;
 	}
 
 	if(rev_sources[symbol] == last_symbol) {
-		// Serial.println("Reverse.");
 		count--;
 	}
-
-	// Serial.println();
 
 	last_symbol = symbol;	
 }
@@ -43,18 +33,15 @@ void isr() {
 void setup() {
 	Serial.begin(115200);
 
-	//Initialize the intterrupt pin (Arduino digital pin 2)
+	// Initialize the interrupt pins (these need to be interrupt-capable).
 	pinMode(PIN_A, INPUT_PULLUP);
 	pinMode(PIN_B, INPUT_PULLUP);
-
 	attachInterrupt(digitalPinToInterrupt(PIN_A), isr, CHANGE);
 	attachInterrupt(digitalPinToInterrupt(PIN_B), isr, CHANGE);
 
 	count = 0;
 }
 void loop() {
-	// ;
-
 	if ( millis() % 10 == 0) {
 		Serial.println(count);
 	}
