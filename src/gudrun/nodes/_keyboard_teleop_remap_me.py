ae7d9fbf63fa54e14ea16cf9e3b7608067031a1a
Tom Bertalan <tom@tombertalan.com>
2019-04-20 22:17:36 -0400

    Use my own teleop.

---
+++ src/gudrun/nodes/_keyboard_teleop_remap_me.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import rospy
+import sys, termios, tty
+from geometry_msgs.msg import Twist
+
+class Teleop(object):
+
+    def __init__(self):
+        rospy.init_node('teleop')
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.message = Twist()
+
+    def publish(self):
+        self.pub.publish(self.message)
+
+    @property
+    def steering(self):
+        return self.message.angular.z
+
+    @steering.setter
+    def steering(self, newvalue):
+        self.message.angular.z = newvalue
+        self.publish()
+
+    @property
+    def throttle(self):
+        return self.message.linear.x
+
+    @throttle.setter
+    def throttle(self, newvalue):
+        self.message.linear.x = newvalue
+        self.publish()
+
+    def stop(self):
+        self.throttle = 0
+    
+    def center(self):
+        self.steering = 0
+
+
+class KeyboardTeleop(Teleop):
+
+    def spin(self):
+        def getch():
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(1)
+         
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
+
+        BUTTON_DELAY = 0.2
+        THROTTLE_INCREMENT = .1
+        STEERING_INCREMENT = .05
+
+        def inc(steering=True, direction=+1):
+            if steering:
+                self.steering += STEERING_INCREMENT * direction
+                print('to', self.steering)
+            else:
+                self.throttle += THROTTLE_INCREMENT * direction
+                print('to', self.throttle)
+
+        keys = dict(
+            A='UP',
+            B='DOWN',
+            C='RIGHT',
+            D='LEFT',
+        )
+        keys['\t'] = 'TAB'
+        keys[' '] = 'SPACE'
+        def display_help():
+            for key, (label, unused_action) in sorted(actions.items()):
+                key = keys.get(key, key)
+                print('"%s": %s' % (key, label))
+            print('============================')
+
+        actions = dict(
+            q=('Quit.', None),
+            w=('Accelerate', lambda : inc(0)),
+            A=('Accelerate', lambda : inc(0)),
+            s=('Decelerate', lambda : inc(0, -1)),
+            B=('Decelerate', lambda : inc(0, -1)),
+            d=('Steer right', lambda : inc(1, -1)),
+            C=('Steer right', lambda : inc(1, -1)),
+            a=('Steer left', lambda : inc(1, 1)),
+            D=('Steer left', lambda : inc(1, 1)),
+            c=('Center steering.', self.center),
+            h=('Display key listing.', display_help),
+            )    
+        actions[' '] = ('Cut throttle.', self.stop)
+        actions['\t'] = ('Cut throttle.', self.stop)
+
+        display_help()
+
+        while True:
+
+            char = getch()
+
+            if char in actions:
+                label, action = actions[char]
+                print(label, end='\n' if action in [None, display_help, self.center, self.stop] else ' ')
+                if action is None:
+                    break
+                action()
+            # else:
+            #     print('Got unknown char "%s".' % char)
+
+
+import multiprocessing
+def _monitor_ping(server, ping_timeout=1):
+    car = Car()
+    while True:
+        timeout = '' if ping_timeout is None else '-w %d -W %d' % (ping_timeout, ping_timeout)
+        cmd = 'ping %s -c 1 %s >/dev/null 2>&1' % (server, timeout)
+        if system(cmd):
+            print('Heartbeat skipped; stopping vehicle.')
+            car.stop()
+            car.center()
+
+
+class Heartbeat(object):
+
+    def __init__(self, server='sindri.vpn.tomsb.net'):
+
+        self.server = server
+        pid = getpid()
+        self.p = multiprocessing.Process(target=_monitor_ping, args=(server,))
+        self.p.start()
+
+    def __del__(self):
+        from os import kill
+        from signal import SIGKILL
+        kill(self.p.pid, SIGKILL)
+        self.p.terminate()
+
+
+
+class MouseTeleop(Teleop):
+
+    def spin(self):
+
+        import pygame
+        import pygame.camera
+        from glob import glob
+
+        pygame.init()
+        RES_X = 320 / 2
+        RES_Y = 240 / 2
+        speed_update_period = .1
+        input_heartbeat_period = 10
+        heartbeat = Heartbeat('sindri.vpn.tomsb.net')
+        
+        display = pygame.display.set_mode((RES_X, RES_Y))
+        pygame.camera.init()
+        camera = pygame.camera.Camera(glob('/dev/video*')[-1], (RES_X, RES_Y))
+        camera.start()
+        screen = pygame.surface.Surface((RES_X, RES_Y), 0, display)
+
+        pygame.event.set_grab(True)
+
+        car = Car()
+
+        last_speed_update_time = time.time()
+
+        with open('teleop.log', 'w') as logfile:
+
+            def log(args, end='\n'):
+
+                logfile.write(
+                    (
+                        str(args) if not isinstance(args, tuple) 
+                        else ' '.join([str(x) for x in args])
+                    ) + end
+                )
+                logfile.flush()
+                sys.stdout.flush()
+                if not isinstance(args, tuple):
+                    args = (args,)
+                print(*args, end=end)
+
+            class ToggleDrivable:
+                def __init__(self):
+                    self.drivable = True
+                    self()
+
+                def __call__(self, stop=None):
+                    if stop:
+                        self.drivable = True
+                    if self.drivable:
+                        log('Stopping car...', end=' ')
+                        car.stop()
+                        car.center()
+                        log('Middle click to enable driving.')
+                    else:
+                        pygame.mouse.set_pos(RES_X/2, RES_Y/2)
+                        pygame.event.set_grab(True)
+                        last_speed_update_time = time.time()
+                        log('Move mouse to drive car.')
+                        log('Middle click or right click to disable.')
+                    self.drivable = not self.drivable
+            toggle_drivable = ToggleDrivable()
+
+            exit = False
+            log('q to exit')
+      
+            while True:
+                screen = camera.get_image(screen)
+                display.blit(screen, (0, 0))
+                pygame.display.flip()
+
+                t = time.time()
+
+                if not pygame.mouse.get_focused() or not pygame.event.get_grab():
+                    if car.throttle != 0:
+                        car.stop()
+                    if car.steering != 0:
+                        car.center()
+                    if toggle_drivable.drivable:
+                        log('Lost focus; stopping car.')
+                        toggle_drivable(True)
+
+                if t - last_speed_update_time > input_heartbeat_period:
+                    if toggle_drivable.drivable:
+                        log('Inputs timed out; stopping car.')
+                        toggle_drivable(True)
+                    else:
+                        last_speed_update_time = t
+
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == ord(" "):
+                            toggle_drivable(True)
+                        elif event.key == pygame.K_ESCAPE:
+                            toggle_drivable(True)
+                            pygame.event.set_grab(False)
+
+                        elif event.key == ord('q') or event.key == pygame.K_ESCAPE:
+                            exit = True
+                        else:
+                            try:
+                                log('Got key %s (%s).' % (event.key, chr(event.key)))
+                            except ValueError:
+                                log('Got non-ASCII keycode %s.' % event.key)
+
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        if event.button == 1: # left button
+                            log('Locked mouse to window (Press esc to halt car and unlock).')
+                            pygame.event.set_grab(True)
+                        elif event.button == 2: # middle button
+                            toggle_drivable()
+                        elif event.button == 3: # right button
+                            toggle_drivable(True)
+                        elif event.button == 8: # web-back
+                            car.MAX_THROTTLE_ABS *= .9
+                            log('Max-abs throttle set to %s.' % car.MAX_THROTTLE_ABS)
+                        elif event.button == 9: # web-fwd
+                            car.MAX_THROTTLE_ABS *= 1.1
+                            log('Max-abs throttle set to %s.' % car.MAX_THROTTLE_ABS)
+                        # button 4 is scroll up; 5 is scroll down
+
+                    elif event.type == pygame.MOUSEMOTION:
+                        c, r = event.pos
+                        if toggle_drivable.drivable:
+                            t = time.time()
+                            elapsed = t - last_speed_update_time 
+                            if elapsed > speed_update_period:
+                                last_speed_update_time = t
+                                car.steering = float(c) / RES_X * 2 - 1
+                                car.throttle = -(float(r) / RES_Y * 2 - 1)
+
+                    elif event.type == pygame.QUIT:
+                        exit = True
+                
+                if exit:
+                    break
+
+
+if __name__ == '__main__':
+
+    kt = KeyboardTeleop()
+    kt.spin()
--- src/gudrun/nodes/teleop.sh
+++ src/gudrun/nodes/teleop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rosrun teleop_twist_keyboard teleop_twist_keyboard.py cmd_vel:=/key_vel
+#rosrun teleop_twist_keyboard teleop_twist_keyboard.py cmd_vel:=/key_vel
+rosrun gudrun _keyboard_teleop_remap_me.py cmd_vel:=/key_vel
